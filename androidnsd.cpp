cc4a8db85d747abcbe79f56d3d62ee56009b7d72
mariuszmaximus <mariusz.maximus@gmail.com>
2023-11-20 20:11:24 +0100

    fix for Qt6

--- androidnsd.cpp
+++ androidnsd.cpp
@@ -53,7 +53,13 @@ QZeroConfPrivate::QZeroConfPrivate(QZeroConf *parent)
 	// There seems to be no straight forward way to match the "thiz" pointer from JNI calls to our pointer of the Java class
 	// Passing "this" as ID down to Java so we can access "this" in callbacks.
 	// Note: needs to be quint64 as uintptr_t might be 32 or 64 bit depending on the system, while Java expects a jlong which is always 64 bit.
-	nsdManager = QJniObject("qtzeroconf/QZeroConfNsdManager", "(JLandroid/content/Context;)V", reinterpret_cast<quint64>(this), QtAndroid::androidActivity().object());
+	nsdManager = QJniObject("qtzeroconf/QZeroConfNsdManager", "(JLandroid/content/Context;)V", reinterpret_cast<quint64>(this),
+	#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+		QtAndroid::androidActivity().object()
+	#else
+		QNativeInterface::QAndroidApplication::context()
+	#endif	
+	);
 	if (nsdManager.isValid()) {
 		jclass objectClass = env->GetObjectClass(nsdManager.object<jobject>());
 		env->RegisterNatives(objectClass, methods, sizeof(methods) / sizeof(methods[0]));
